6da4efd8f0f4b43e8b82e9d239bfa75460ccc612
lzhang8102 <120515923085@qq.com>
2018-09-26 15:32:53 +0800

    update

--- send_email.py
+++ send_email.py
@@ -1,35 +1,7 @@
-# coding: utf-8
-import unittest
-from common import HTMLTestRunner_cn
-import time
+# coding:utf-8
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
-# 用例地址
-tc_dir = "D:\\test\\case"
-# 测试套件收集用例
-# discover方法加载多个用例集合
-discover = unittest.defaultTestLoader.discover(start_dir=tc_dir,
-                                               pattern="aws*.py",
-                                               top_level_dir=None)
-
-print(discover)
-
-if __name__ == "__main__":
-    # 生成报告的路径及时间
-    report_path = "D:\\thiscomputer\\workspace\\aws_test\\report\\"+"result.html"
-    # 打开报告并写入测试结果
-    fp = open(report_path,"wb")
-    runner = HTMLTestRunner_cn.HTMLTestRunner(stream=fp,
-                                              title=u"大数据平台AWS测试报告",
-                                              description=u"用例执行结果")
-
-    # 运行测试用例
-    runner.run(discover)
-    # 关闭报告文件
-    fp.close()
-
 
 # --------1.跟发件相关的参数------
 smtserver = "smtp.qq.com"      # 发件服务器
